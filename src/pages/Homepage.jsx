b8923d3619d268b1aac50b3a91870564b903d8e3
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-08 01:32:56 +0200

    [UPD] homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,12 @@
 const Homepage = () => {
   return (
     <>
-      <div>
-        <h1>Homepage</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="text-center">Benvenuto nel mio E-Commerce</h1>
+          </div>
+        </div>
       </div>
     </>
   );
