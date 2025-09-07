061b326064a7d4a628a755291f93aeca8dc2a082
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-08 01:21:15 +0200

    [UPD] rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Defaultlayout />}>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/products" Component={Products}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
